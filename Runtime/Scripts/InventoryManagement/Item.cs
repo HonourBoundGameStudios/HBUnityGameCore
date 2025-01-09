723d40818a61670a53c0084d95f81c48e19bc260
Richard Lalancette <richard.lalancette@gmail.com>
2025-01-09 16:48:13 -0500

    Added Melee Weapon Class

--- Runtime/Scripts/InventoryManagement/Item.cs
+++ Runtime/Scripts/InventoryManagement/Item.cs
@@ -6,41 +6,37 @@ using UnityEngine.Localization;
 [CreateAssetMenu(fileName = "New Basic Item", menuName = "Inventory/Item")]
 public class Item : ScriptableObject
 {
-    // The icon of the item that will be displayed inspector ui.
     [Header("VISUAL REPRESENTATION")]
+    [SerializeField]
+    public Sprite icon; // The icon of the item that will be displayed in the inspector ui.
 
     [SerializeField]
-    public Sprite icon;
+    public GameObject pickupObject; // 2D or 3D object to display in the world
 
-    // The prefab of the item that will be spawned in the world when the player picks it up.
     [SerializeField]
-    public GameObject pickupObject;
-
-    // The prefab of the item that will be displayed in the ui (Typically a 3D model).
-    [SerializeField]
-    public GameObject displayObject;
+    public GameObject displayObject; // 2D or 3D object to display in the UI
 
     [Header("DETAILS (Localized Fields)")]
     [SerializeField]
-    public String localizationTableName;
+    public String localizationTableName; // The name of the Localisation table.
 
     [Header("TITLE")]
     [SerializeField]
-    public String titleLocalizationKey;
+    public String titleLocalizationKey; // Key used to look up the description in the localization table
 
     [SerializeField]
     public LocalizedString titleLocalized;
 
-    private String title;
+    private String title; // Autofill from the localized string. Not persisted.
 
     [Header("DESCRIPTION")]
     [SerializeField]
-    public String descriptionLocalizationKey;
+    public String descriptionLocalizationKey; // Key used to look up the description in the localization table
 
     [SerializeField]
     public LocalizedString descriptionLocalized;
 
-    private String description;
+    private String description; // Autofill from the localized string. Not persisted
 
     [SerializeField]
     public bool unique; // A single instance of this item can exist in the game
@@ -54,11 +50,14 @@ public class Item : ScriptableObject
     [SerializeField]
     public bool consumable; // The item can be consumed by the player
 
-    // All steps of implementation this item a done and
-    // the item is ready to be added to the world (Ready to be used in game)
-    // This is a flag for the developers to know what is ready to be used
     [SerializeField]
-    public bool markAsImplemented;
+    public float weight; // The weight of the item in the inventory
+
+    [SerializeField]
+    public float value; // The value of the item in the currency of the game
+
+    [SerializeField]
+    public bool markAsImplemented; // This is a flag for the developers to know what is ready to be used
 
     private void Start()
     {
@@ -80,80 +79,4 @@ public class Item : ScriptableObject
     {
         description = updatedString;
     }
-
-    // CRAFTING TODO
-    //
-    // [SerializeField, Range(1, 50)]
-    // public int craftedQty = 1;
-    //
-    // public InventoryByQty craftingMaterials;
-    //
-    // public bool CanBeCrafted()
-    // {
-    //     return PlayerInventory.current.CombinedInventory().ContainsItems(craftingMaterials);
-    // }
-    //
-    // public bool CanBeCrafted(int multiplier)
-    // {
-    //     return PlayerInventory.current.CombinedInventory().ContainsItems(craftingMaterials, multiplier);
-    // }
-    //
-    // public int GetItemCraftingOrder()
-    // {
-    //     if (CanBeCrafted())
-    //     {
-    //         switch (GetItemType())
-    //         {
-    //             case EItemType.DrillAttachment:
-    //                 return 0;
-    //             case EItemType.Gun:
-    //             case EItemType.GloveAttachment:
-    //                 return 1;
-    //             case EItemType.ClunkyUpgrade:
-    //             case EItemType.Melee:
-    //             case EItemType.ScannerAttachment:
-    //                 return 2;
-    //         }
-    //         return 3;
-    //     }
-    //     else
-    //     {
-    //         return 4;
-    //     }
-    // }
-    // public ItemBlueprint RequiredBlueprint()
-    // {
-    //     return craftingMaterials.GetItem(0) as ItemBlueprint;
-    // }
-
-
-    // ┌───────────────────────────────────────────────────────────────────────────────────────────────────────────┐
-    // │ EVENTS ON COLLECTED                                                                                       │
-    // └───────────────────────────────────────────────────────────────────────────────────────────────────────────┘
-
-    // [SerializeField]
-    // public ItemGuide givenGuide;
-
-    // [SerializeField]
-    // public ObjectiveScriptable objectiveCompleted;
-
-    // [SerializeField]
-    // public ScriptableObjects.SoundEffectSO defaultPickupSound;
-
-    // // ┌───────────────────────────────────────────────────────────────────────────────────────────────────────────┐
-    // // │ ITEM SORTING                                                                                              │
-    // // └───────────────────────────────────────────────────────────────────────────────────────────────────────────┘
-    // public static void SortItemsByType(ref List<Item> items)
-    // {
-    //     items.Sort((a, b) =>
-    //     {
-    //         int typeCompare = a.GetItemTypeDefaultOrder().CompareTo(b.GetItemTypeDefaultOrder());
-    //
-    //         if (typeCompare == 0)
-    //             typeCompare = a.GetDisplayName().CompareTo(b.GetDisplayName());
-    //
-    //         return typeCompare;
-    //     });
-    // }
-
 }
