609c9adbb27e5bc7663cb11bcf603f84e2a17ebc
Richard Lalancette <richard.lalancette@gmail.com>
2025-01-30 20:49:53 -0500

    Update EntityClass.cs
    Fixed CreateAssetMenu Path.

--- Runtime/Scripts/EntityClassSystem/EntityClass.cs
+++ Runtime/Scripts/EntityClassSystem/EntityClass.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace HBUnityGameCore
 {
     [Serializable]
-    [CreateAssetMenu(fileName = "Entity Class", menuName = "HBUnityGameCore//EntityClass")]
+    [CreateAssetMenu(fileName = "Entity Class", menuName = "HBUnityGameCore/EntityClass")]
     public class EntityClass : BaseObjectScriptableObject
     {
         SerializableDictionary<string, Skill> _skills;
